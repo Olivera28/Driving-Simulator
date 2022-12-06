10670e0eac61734d7217ca93273a5cd43f57ae4e
Olivera28 <103554585+Olivera28@users.noreply.github.com>
2022-12-06 13:50:31 -0500

    Create drive.py

---
+++ Python scripts/drive.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+drive_f = 21
+drive_b = 20
+
+turn_r = 16
+turn_l = 26
+
+GPIO.setwarnings(False)	
+
+GPIO.setmode(GPIO.BCM) # Choose BCM to use GPIO numbers instead of pin numbers (BOARD)
+
+GPIO.setup(drive_f,GPIO.OUT)
+GPIO.setup(drive_b,GPIO.OUT)
+GPIO.setup(turn_r,GPIO.OUT)
+GPIO.setup(turn_l,GPIO.OUT)
+
+while True:
+    val = input("Enter command: ")
+    
+    if val == "r":
+        GPIO.output(drive_f,0)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,1)
+        GPIO.output(turn_l,0)
+        sleep(1)
+
+    elif val == "l":
+        GPIO.output(drive_f,0)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,1)
+        sleep(1)
+        
+    elif val == "c":
+        GPIO.output(drive_f,0)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,0)
+        sleep(1)
+        
+    elif val == "d":
+        GPIO.output(drive_f,1)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,0)
+        sleep(1)
+        
+    elif val == "b":
+        GPIO.output(drive_f,0)
+        GPIO.output(drive_b,1)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,0)
+        sleep(1)
+        
+        
+    elif val == "dr":
+        GPIO.output(drive_f,1)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,0)
+        sleep(3)
+        GPIO.output(drive_f,1)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,1)
+        GPIO.output(turn_l,0)
+        sleep(1)
+            
+    elif val == "dl":
+        GPIO.output(drive_f,1)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,0)
+        sleep(3)
+        GPIO.output(drive_f,1)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,1)
+        sleep(1)     
+    
+    else:
+        GPIO.output(drive_f,0)
+        GPIO.output(drive_b,0)
+        GPIO.output(turn_r,0)
+        GPIO.output(turn_l,0)
+        sleep(1)
+    
+    
+    #time.sleep(.25)
+    #GPIO.output(pin,False)
+    #time.sleep(.25)
+        
+    
+GPIO.cleanup()
